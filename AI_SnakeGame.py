5af30dd1f42ec70f25d8b7b83c7dec4260d58cee
Shubham Pawar <pawarshubham.0610@gmail.com>
2025-06-30 20:02:03 +0530

    Update AI_SnakeGame.py

--- AI_SnakeGame.py
+++ AI_SnakeGame.py
@@ -1,14 +1,32 @@
 from snake.game import Game, GameConf, GameMode
 
-greedy = "GreedySolver"
-hamilton = "HamiltonSolver"
+DQN_SOLVER = "DQNSolver" 
 
-normal = GameMode.NORMAL
-
+MODE = GameMode.TRAIN_DQN_GUI 
 
 conf = GameConf()
-conf.solver_name = hamilton
-conf.mode = normal
-print("Solver: %s " % (conf.solver_name))
-print("Mode: %s" %(conf.mode))
+conf.solver_name = DQN_SOLVER
+conf.mode = MODE
+
+
+print("\n=== Snake RL Research Experiment ===")
+print(f"Solver: {conf.solver_name}")
+print(f"Mode: {conf.mode}")
+print(f"Map size: {conf.map_rows}x{conf.map_cols}")
+print("====================================\n")
+
 Game(conf).run()
+
+from snake.solver.base import SolverBase
+
+class PPOAgent(SolverBase):
+    def __init__(self, ...):
+        super().__init__(...)
+       
+
+    def select_action(self, state):
+        pass
+
+    def train(self, ...):
+        # PPO training loop
+        pass
